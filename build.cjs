061442c649f9a66ffadc56c840040eec7e645c17
Pcrab <pcrab@pcrab.xyz>
2023-03-29 21:15:52 +0800

    feat: lots of rules

---
+++ build.cjs
@@ -0,0 +1,9 @@
+const esbuild = require("esbuild");
+
+esbuild.build({
+    entryPoints : ["src/index.cjs"],
+    bundle      : true,
+    outfile     : "dist/index.cjs",
+    format      : "cjs",
+    minify      : true,
+});
